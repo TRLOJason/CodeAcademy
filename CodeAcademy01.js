622629c34d002657fb581c130e9733b64acf7053
TRLOJason <deloric@trlo.me>
2015-02-05 14:41:16 -0500

    2

--- CodeAcademy01.js
+++ CodeAcademy01.js
@@ -17,11 +17,22 @@ console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace m
 
 console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
 
-var userAnswer = prompt("Do you want to race Bieber on stage?");
+var userAnswer = prompt("Do you want to race Bieber on stage? yes/no");
 
-if userAnswer === "yes" 
+if (userAnswer = "yes") 
 {
     console.log("You and Bieber start racing. It's neck and neck! You win by a shoelace!");
 } else {
     console.log("Oh no! Bieber shakes his head and sings 'I set a pace, so I can race without pacing.'");
+}
+
+var feedback = prompt("Please rate my game between 1 and 10");
+
+if (feedback >= 8)
+{
+    console.log("Thank you! We should race at the next concert!");
+}
+else 
+{
+    console.log("I'll keep practicing coding and racing.");
 }
